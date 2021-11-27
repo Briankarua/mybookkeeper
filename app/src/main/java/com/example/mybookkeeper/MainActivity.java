14b280b4c45ffcede005e378abb9042d91792902
Karua Muthama <karwamutha@gmail.com>
2021-11-27 20:14:43 +0300

    :tada: Initial Commit

--- app/src/main/java/com/example/mybookkeeper/MainActivity.java
+++ app/src/main/java/com/example/mybookkeeper/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Add comment Example
         setContentView(R.layout.activity_main);
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setVisibility(View.GONE);
